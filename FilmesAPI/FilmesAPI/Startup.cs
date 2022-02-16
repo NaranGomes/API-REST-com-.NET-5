918c0f425e40d52f4f8f9ce90941b78cd1a2c8a8
Naran Gomes <gom3snaran@gmail.com>
2022-02-16 17:06:54 -0300

    Aplicando AutoMapper com os Dtos

--- FilmesAPI/FilmesAPI/Startup.cs
+++ FilmesAPI/FilmesAPI/Startup.cs
@@ -33,7 +33,8 @@ namespace FilmesAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmesAPI", Version = "v1" });
-            });            
+            });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
