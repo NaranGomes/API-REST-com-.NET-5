86b768f4b49ef34d24fe7fbe90bbaf34391cb823
Naran Gomes <gom3snaran@gmail.com>
2022-02-17 14:24:36 -0300

    Criando a Entidade Gerente

---
+++ FilmesAPI/FilmesAPI/Controllers/GerenteController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos.Gerente;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GerenteController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GerenteController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaGerente(CreateGerenteDto dto)
+        {
+            var gerente = _mapper.Map<Gerente>(dto);
+            _context.Gerentes.Add(gerente);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaGerentesPorId), new { Id = gerente.Id }, gerente);
+        }
+
+        [HttpGet("{id}")]
+        private IActionResult RecuperaGerentesPorId(int id)
+        {
+            Gerente gerente = _context.Gerentes.FirstOrDefault(gerente => gerente.Id == id);
+            if (gerente != null)
+            {
+                ReadGerenteDto gerenteDto = _mapper.Map<ReadGerenteDto>(gerente);
+
+                return Ok(gerenteDto);
+            }
+            return NotFound();
+        }        
+    }
+}
