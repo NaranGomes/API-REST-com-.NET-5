cb0763841950d9fb63ff60b6a3540f6ec163b53c
Naran Gomes <gom3snaran@gmail.com>
2022-02-18 14:34:26 -0300

    Trazendo informacoes adicionais com automapper em tempo de execucao

--- FilmesAPI/FilmesAPI/Data/Dtos/Sessao/ReadSessaoDto.cs
+++ FilmesAPI/FilmesAPI/Data/Dtos/Sessao/ReadSessaoDto.cs
@@ -1,4 +1,5 @@
 ﻿using FilmesAPI.Models;
+using System;
 
 namespace FilmesApi.Data.Dtos.Sessao
 {
@@ -6,6 +7,8 @@ namespace FilmesApi.Data.Dtos.Sessao
     {
         public int Id { get; set; }
         public virtual Cinema Cinema { get; set; }
-        public virtual Filme Filme { get; set; }                
+        public virtual Filme Filme { get; set; }
+        public DateTime HorarioDeEncerramento { get; set; }
+        public DateTime HorarioDeInicio { get; set; }
     }
 }
