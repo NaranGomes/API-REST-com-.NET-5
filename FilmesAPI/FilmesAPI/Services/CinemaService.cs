607fd4d9a63b0791ca642d74551bcb62e0618c9d
Naran Gomes <gom3snaran@gmail.com>
2022-02-22 14:15:56 -0300

    Metodos AtulizaFilme e DeletaFilme e FilmeService e metodos AtualizaCinema e DeletaCinema em CinemaService

--- FilmesAPI/FilmesAPI/Services/CinemaService.cs
+++ FilmesAPI/FilmesAPI/Services/CinemaService.cs
@@ -2,6 +2,7 @@
 using FilmesApi.Data;
 using FilmesAPI.Data.Dtos;
 using FilmesAPI.Models;
+using FluentResults;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -55,6 +56,30 @@ namespace FilmesApi.Services
                 return cinemaDto;
             }
             return null;
+        }
+
+        public Result DeletaCinema(int id)
+        {
+            Cinema cinema = _context.Cinemas.FirstOrDefault(cinema => cinema.Id == id);
+            if (cinema == null)
+            {
+                return Result.Fail("Cinema não encontrado");
+            }
+            _context.Remove(cinema);
+            _context.SaveChanges();
+            return Result.Ok();
+        }
+
+        public Result AtualizaCinema(int id, UpdateCinemaDto cinemaDto)
+        {
+            Cinema cinema = _context.Cinemas.FirstOrDefault(cinema => cinema.Id == id);
+            if (cinema == null)
+            {
+                return Result.Fail("Cinema não encontrado");
+            }
+            _mapper.Map(cinemaDto, cinema);
+            _context.SaveChanges();
+            return Result.Ok();
         }
     }
 }
