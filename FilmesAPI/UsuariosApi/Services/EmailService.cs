7b492add97ab426b1b79cf901c58523d3e936201
Naran Gomes <gom3snaran@gmail.com>
2022-02-25 23:33:13 -0300

    Metodo Cria corpo do Email Criado

--- FilmesAPI/UsuariosApi/Services/EmailService.cs
+++ FilmesAPI/UsuariosApi/Services/EmailService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
 using UsuariosApi.Models;
 
 namespace UsuariosApi.Services
@@ -8,6 +10,44 @@ namespace UsuariosApi.Services
         public void EnviarEmail(string[] destinatario, string assunto, int usuarioId, string code)
         {
             Mensagem mensagem = new Mensagem(destinatario, assunto, usuarioId, code);
+            var mensagemDeEmail = CriaCorpoDoEmail(mensagem);
+            Enviar(mensagemDeEmail);
+
+        }
+
+        private void Enviar(MimeMessage mensagemDeEmail)
+        {
+            using(var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect("Conexão a Fazer");
+                    //TODO Autenticação de e-mail
+                    client.Send(mensagemDeEmail);
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        private MimeMessage CriaCorpoDoEmail(Mensagem mensagem)
+        {
+            var mensagemDeEmail = new MimeMessage();
+            mensagemDeEmail.From.Add(new MailboxAddress("ADICIONAR O REMETENTE"));
+            mensagemDeEmail.To.AddRange(mensagem.Destinatario);
+            mensagemDeEmail.Subject = mensagem.Assunto;
+            mensagemDeEmail.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = mensagem.Conteudo
+            };
+            return mensagemDeEmail;
         }
     }
 }
