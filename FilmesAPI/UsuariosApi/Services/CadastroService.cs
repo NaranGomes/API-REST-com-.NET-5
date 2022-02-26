646ab7eb0fe3dca35832a986ad62aaed6b9c30a9
Naran Gomes <gom3snaran@gmail.com>
2022-02-25 22:59:06 -0300

    Classe EmailService, Mensagem e metodo EnviaEmail Criados

--- FilmesAPI/UsuariosApi/Services/CadastroService.cs
+++ FilmesAPI/UsuariosApi/Services/CadastroService.cs
@@ -14,11 +14,15 @@ namespace UsuariosApi.Services
     {
         private IMapper _mapper;
         private UserManager<IdentityUser<int>> _userManager;
+        private EmailService _emailService;
 
-        public CadastroService(IMapper mapper, UserManager<IdentityUser<int>> userManager)
+        public CadastroService(IMapper mapper,
+            UserManager<IdentityUser<int>> userManager,
+            EmailService emailService)
         {
             _mapper = mapper;
             _userManager = userManager;
+            _emailService = emailService;
         }
 
         public Result CadastraUsusario(CreateUsuarioDto createDto)
@@ -29,6 +33,10 @@ namespace UsuariosApi.Services
             if (resultadoIdentity.Result.Succeeded)
             {
                 var code = _userManager.GenerateEmailConfirmationTokenAsync(usuarioIdentity).Result;
+                
+                _emailService.EnviarEmail(new [] { usuarioIdentity.Email },
+                    "Link de ativação",  usuarioIdentity.Id, code
+                    );
                 return Result.Ok().WithSuccess(code);
             }            
             return Result.Fail("Falha ao cadastrar o usuário");
---
+++ FilmesAPI/UsuariosApi/Services/EmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Services
+{
+    public class EmailService
+    {
+        public void EnviarEmail(string[] destinatario, string assunto, int usuarioId, string code)
+        {
+            Mensagem mensagem = new Mensagem(destinatario, assunto, usuarioId, code);
+        }
+    }
+}
