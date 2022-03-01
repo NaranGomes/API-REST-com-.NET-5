2adbec600b9fe1285618d4ad7b638113f6536dd3
Naran Gomes <gom3snaran@gmail.com>
2022-03-01 19:29:57 -0300

    Metodo ResetaSenhaUsuario na Classe LoginService implementado

--- FilmesAPI/UsuariosApi/Services/LoginService.cs
+++ FilmesAPI/UsuariosApi/Services/LoginService.cs
@@ -34,16 +34,31 @@ namespace UsuariosApi.Services
 
         public Result SolicitaResetSenhaUsuario(SolicitaResetRequest request)
         {
-            IdentityUser<int> identityUser = _signInManager.UserManager.Users
-                .FirstOrDefault(u => u.NormalizedEmail == request.Email.ToUpper());
+            IdentityUser<int> identityUser = RecuperaUsuarioPorEmail(request.Email);
 
-            if(identityUser != null)
+            if (identityUser != null)
             {
                 string codigoDeRecuperacao = _signInManager.UserManager
                     .GeneratePasswordResetTokenAsync(identityUser).Result;
                 return Result.Ok().WithSuccess(codigoDeRecuperacao);
             }
             return Result.Fail("Falhar ao solicitar Redefinição");
+        }       
+
+        public Result ResetaSenhaUsuario(EfetuaResetRequest request)
+        {
+            IdentityUser<int> identityUser = RecuperaUsuarioPorEmail(request.Email);
+
+            IdentityResult resultadoIdentity = _signInManager.UserManager
+                .ResetPasswordAsync(identityUser, request.Token, request.Password).Result;
+            if (resultadoIdentity.Succeeded) return Result.Ok()
+                     .WithSuccess("Senha redefinida com sucesso");
+            return Result.Fail("Houve um erro na operação");
+        }
+        private IdentityUser<int> RecuperaUsuarioPorEmail(string email)
+        {
+            return _signInManager.UserManager.Users
+                    .FirstOrDefault(u => u.NormalizedEmail == email.ToUpper());
         }
     }
 }
